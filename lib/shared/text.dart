6c904ec8b01d6ee5741346a887e550ca3ee0c132
Rochmad <rochmad.wa@gmail.com>
2023-05-13 15:23:15 +0700

    feat: update the content again

--- lib/shared/text.dart
+++ lib/shared/text.dart
@@ -4,31 +4,29 @@ const String tumbaspedia = "Tumbaspedia adalah sebuah aplikasi jual-beli bagi UM
 const String kopimi =
     "Gerai Kopimi merupakan singkatan dari Gerakan Terintegrasi Koperasi dan Usaha Mikro, Kecil, dan Menengah yang merupakan forum bagi usaha mikro, kecil, dan menengah di Kota Semarang. Forum ini dibentuk berdasarkan SK Walikota No 518/1255 Tahun 2019 dan memiliki kepengurusan mulai dari tingkat kota, kecamatan hingga kelurahan.";
 
-const String prosesPesanan = "Langkah-langkah proses pesanan:\n" +
-    "1. Anda menerima pesanan dari Pembeli.\n" +
-    "2. Hubungi pembeli untuk menyetujui pembayaran dan pengiriman barang.\n" +
-    "3. Setelah kesepakatan tercapai, ubah status pesanan menjadi \"Pesanan sedang diproses\".\n" +
-    "4. Proses dan kirim pesanan sesuai dengan kesepakatan.\n" +
-    "5. Setelah pesanan sampai di tangan pembeli, ubah status pesanan menjadi \"Pesanan telah diterima\".\n" +
+const String prosesPesanan = "Langkah-langkah proses pesanan:\n\n" +
+    "1. Anda menerima pesanan dari Pembeli.\n\n" +
+    "2. Hubungi pembeli untuk menyetujui pembayaran dan pengiriman barang.\n\n" +
+    "3. Setelah kesepakatan tercapai, ubah status pesanan menjadi \"Pesanan sedang diproses\".\n\n" +
+    "4. Proses dan kirim pesanan sesuai dengan kesepakatan.\n\n" +
+    "5. Setelah pesanan sampai di tangan pembeli, pembeli mengubah status pesanan menjadi \"Pesanan telah diterima\".\n\n" +
     "6. Pembeli harus melakukan konfirmasi penerimaan barang agar status pesanan menjadi \"Pesanan selesai\".";
 
-const String buatAkun = "Cara membuat akun di Tumbaspedia:\n" +
-    "1. Klik tombol \"Daftar\" pada halaman pendaftaran.\n" +
-    "2. Isi formulir pendaftaran dengan data diri yang valid, seperti nama lengkap, email, nomor telepon, dan password. Setelah itu, klik tombol \"Lanjutkan\".\n" +
-    "3. Masukkan informasi mengenai toko Anda, seperti nama toko, alamat, deskripsi, nomor NIB dan foto NIB, serta gambar toko. Pastikan data yang dimasukkan sudah benar dan valid. Setelah selesai, klik tombol \"Daftar\".\n" +
-    "4. Lakukan konfirmasi melalui email yang telah dikirimkan ke alamat email yang didaftarkan.\n" +
+const String buatAkun = "Cara membuat akun di Tumbaspedia:\n\n" +
+    "1. Klik tombol \"Daftar\" pada halaman pendaftaran.\n\n" +
+    "2. Isi formulir pendaftaran dengan data diri yang valid, seperti nama lengkap, email, nomor telepon, dan password. Setelah itu, klik tombol \"Lanjutkan\".\n\n" +
+    "3. Masukkan informasi mengenai toko Anda, seperti nama toko, alamat, deskripsi, nomor NIB dan foto NIB, serta gambar toko. Pastikan data yang dimasukkan sudah benar dan valid. Setelah selesai, klik tombol \"Daftar\".\n\n" +
+    "4. Lakukan konfirmasi melalui email yang telah dikirimkan ke alamat email yang didaftarkan.\n\n" +
     "5. Setelah melakukan konfirmasi melalui email, akun Tumbaspedia Anda akan langsung aktif dan siap digunakan untuk mengatur toko Anda, menambahkan produk, dan melakukan transaksi dengan pembeli.";
 
-const String statusPesanan = "Status pesanan Anda dapat diubah dengan kesepakatan penjual. Berikut adalah beberapa status yang mungkin terjadi:\n" +
-    "1. Pesanan baru: Status awal ketika Anda pertama kali memesan produk di toko.\n" +
-    "2. Pesanan dibatalkan: Apabila Anda atau penjual membatalkan pesanan.\n" +
-    "3. Pesanan sedang diproses: Status pesanan setelah kesepakatan antara pembeli dan penjual tercapai, dan penjual mulai memproses pesanan. \n" +
-    "4. Pesanan dalam pengiriman: Status yang menunjukkan bahwa pesanan sudah dikirim oleh penjual dan sedang dalam proses pengiriman.\n" +
-    "5. Pesanan telah diterima: Status yang menunjukkan bahwa pesanan sudah diterima oleh pembeli.\n" +
-    "6. Pesanan selesai: Status akhir untuk penjual setelah pesanan telah diterima oleh pembeli dan pembeli mengkonfirmasi penerimaan barang melalui tombol \"Terima pesanan\" pada detail pesanan dengan status \"Pesanan dalam pengiriman\".";
+const String statusPesanan = 'Status pesanan Anda dapat diubah dengan kesepakatan penjual. Berikut adalah beberapa status yang mungkin terjadi:\n\n' +
+    '1. Pesanan baru: Status awal ketika Anda pertama kali memesan produk di toko.\n\n' +
+    '2. Pesanan dibatalkan: Apabila Anda atau penjual membatalkan pesanan.\n\n' +
+    '3. Pesanan sedang diproses/pengiriman: Status pesanan setelah kesepakatan antara pembeli dan penjual tercapai, dan penjual mulai memproses pesanan dan dalam proses pengiriman.\n\n' +
+    '4. Pesanan selesai: Status akhir untuk penjual setelah pesanan telah diterima oleh pembeli dan pembeli mengkonfirmasi penerimaan barang melalui tombol "Terima pesanan" pada detail pesanan dengan status "Pesanan sedang diproses/pengiriman".';
 
 const String tandaMerah =
-    "Tanda notifikasi berwarna merah pada aplikasi menunjukkan bahwa ada pesanan baru yang masuk untuk penjual. Kemudian penjual dapat memprosesnya sesuai dengan kebutuhan.";
+    "Tanda notifikasi berwarna merah pada aplikasi Tumbaspedia menunjukkan bahwa terdapat pesanan baru yang masuk untuk penjual. Setelah menerima notifikasi tersebut, penjual dapat memproses pesanan sesuai dengan kebutuhan. Pastikan untuk selalu memeriksa notifikasi secara berkala agar tidak melewatkan pesanan baru.";
 
 const String help =
     "Jika Anda mengalami masalah saat mengakses aplikasi, mendaftar akun, atau toko, silakan hubungi admin Tumbaspedia melalui akun Facebook atau Instagram Gerai Kopimi Rowosari. Jika terjadi kesalahan sistem, silakan hubungi pengembang aplikasi melalui email rochmad.wa@gmail.com.";
